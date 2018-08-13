ad1b660f0dabc307743b72cf6dc58e12a1b7a1bd
Brian Nelson and Kenneth du <brian@>
2018-08-13 14:30:34 -0700

    add button to show all cards of a 52 card deck

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,17 @@
 $(document).ready(function() {
 
 
-  $("#groceryList").submit(function(event) {
-    event.preventDefault();
+  $("button#deck").click(function() {
 
-    var gLists = ["item1", "item2", "item3", "item4", "item5", "item6"];
-    var inputList = [];
-    gLists.forEach(function(gList) {
-      var itemList = $("input#" + gList).val();
+    var cardNumber = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+    var cardSuit = ["Hearts", "Diamonds", "Clubs", "Spades"];
 
-      inputList.push(itemList);
-    })
-    // var item2 = $("input#item2").val();
-    // var item3 = $("input#item3").val();
-    // var item4 = $("input#item4").val();
-    // var item5 = $("input#item5").val();
-
-    var preSort = [];
-    // gLists = gLists.sort();
-
-    preSort = inputList.sort();
-
-
-    var upperCase = [];
-
-    preSort.forEach(function(word) {
-    var upperWord = word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
-    upperCase.push(upperWord);
-
-  })
-
-
-    upperCase.forEach(function(glist) {
-      $(".output ul").append("<li>" + glist + "</li>");
+    cardSuit.forEach(function(suit) {
+      cardNumber.forEach(function(number) {
+        var output = (number + " of " + suit + "<br>");
+        $(".output").append(output);
+      });
     });
 
   });
-
-
 });
